9f0fa02dc917e7f7bedb38efd2feef74965fee62
Yvan NEDELEC <yvan.nedelec@etu.univ-poitiers.fr>
2025-09-15 11:50:48 +0200

    ajout de navbar

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
+# app.py
+
 from dash import Dash, html
 import dash
 import dash_bootstrap_components as dbc
 from flask_caching import Cache
 
-# App multipages (conforme TD3: use_pages=True)
-app = Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.FLATLY], title="Car Annotation App")
+app = Dash(__name__, use_pages=True,
+           external_stylesheets=[dbc.themes.FLATLY],
+           title="Car Annotation App")
 server = app.server
 
-# Cache disque pour accélérer stats/IAA
 cache = Cache(app.server, config={
     "CACHE_TYPE": "FileSystemCache",
     "CACHE_DIR": ".cache",
@@ -15,21 +17,23 @@ cache = Cache(app.server, config={
 })
 app.server.config["APP_CACHE"] = cache
 
-# Barre de navigation et container pages
+# 🔽 Génération dynamique des liens vers toutes les pages
+nav_links = [
+    dbc.NavItem(dbc.NavLink(page["name"], href=page["path"]))
+    for page in dash.page_registry.values()
+]
+
 app.layout = html.Div([
     dbc.Navbar(
         dbc.Container([
             dbc.NavbarBrand("🚗 Car Annotation App", className="navbar-brand"),
-            dbc.Nav(className="ms-auto", children=[
-                html.Span("Raccourcis : "), html.Kbd("N"), html.Span(" / "),
-                html.Kbd("P"), html.Span(" / "), html.Kbd("S"),
-            ],)
+            dbc.Nav(nav_links, className="ms-auto"),
         ]), color="primary", dark=True
     ),
     dbc.Container([dash.page_container], fluid=True)
 ])
 
-# Raccorde les callbacks définis dans les pages
+# Raccorde les callbacks
 from pages import annotate as pg_annotate, review as pg_review, stats as pg_stats
 pg_annotate.register_callbacks(app)
 pg_review.register_callbacks(app)
